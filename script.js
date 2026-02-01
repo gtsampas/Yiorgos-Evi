177b7fff2ae37476a877b36826b93ce0768c2577
gtsampas <41155510+gtsampas@users.noreply.github.com>
2026-02-01 20:05:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const envelope = document.getElementById('envelope');
 
 envelope.addEventListener('click', () => {
-  envelope.classList.add('open');
-});
-
-window.addEventListener('scroll', () => {
-  envelope.classList.add('open');
+  envelope.classList.toggle('open');
 });
